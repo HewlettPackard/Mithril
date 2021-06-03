a45260868f26d992bbae3bca4cb6f95fee34a02a
Max Lambrecht <maxlambrecht@gmail.com>
2021-06-03 11:42:59 -0300

    Add some util commands.
    Signed-off-by: Max Lambrecht <maxlambrecht@gmail.com>

--- POC/utils.sh
+++ POC/utils.sh
@@ -35,4 +35,23 @@ curl localhost:15000/certs
 # Check Envoy proxy configuration:
 curl localhost:15000/config_dump
 
+# Change logging config to debug:
+curl -X POST localhost:15000/logging?level=debug
 
+
+# Port forward to the first istio-ingressgateway pod
+alias igpf='kubectl -n istio-system port-forward $(kubectl -n istio-system get pods -listio=ingressgateway -o=jsonpath="{.items[0].metadata.name}") 15000'
+
+# Get the http routes from the port-forwarded ingressgateway pod (requires jq)
+alias iroutes='curl --silent http://localhost:15000/config_dump | jq '\''.configs.routes.dynamic_route_configs[].route_config.virtual_hosts[]| {name: .name, domains: .domains, route: .routes[].match.prefix}'\'''
+
+# Get the logs of the first istio-ingressgateway pod
+# Shows what happens with incoming requests and possible errors
+alias igl='kubectl -n istio-system logs $(kubectl -n istio-system get pods -listio=ingressgateway -o=jsonpath="{.items[0].metadata.name}") --tail=300'
+
+# Get the logs of the first istio-pilot pod
+# Shows issues with configurations or connecting to the Envoy proxies
+alias ipl='kubectl -n istio-system logs $(kubectl -n istio-system get pods -listio=pilot -o=jsonpath="{.items[0].metadata.name}") discovery --tail=300'
+
+# Debug services connections
+kubectl run --generator=run-pod/v1 -i --tty busybox-curl --image=radial/busyboxplus:curl --restart=Never -- sh

3904b8192e156530cb91c85b8454bd946d93159b
nathalia-satie.gomazako@hpe.com <nathalia-satie.gomazako@hpe.com>
2021-07-21 14:41:42 -0300

    [ISTIOSPIRE-37] Remove istio-namespace yaml. Modify create-docker-registry-secret.sh

--- POC/create-docker-registry-secret.sh
+++ POC/create-docker-registry-secret.sh
@@ -2,10 +2,12 @@
 
 HUB_PASSWORD="$(aws ecr get-login-password --region us-east-1)"
 
-kubectl apply -f istio/istio-namespace.yaml 
+kubectl create ns istio-system
 
 kubectl create secret docker-registry secret-registry -n istio-system \
   --docker-server=$HUB\
   --docker-username=AWS \
   --docker-password=$HUB_PASSWORD
   
+#copy docker-registry secret to namespace default 
+kubectl get secret secret-registry --namespace=istio-system -o yaml | sed 's/namespace: istio-system/namespace: default/g' | kubectl create -f -  

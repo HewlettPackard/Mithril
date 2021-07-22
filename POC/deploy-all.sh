38008cac4381993c47e391e630c9f42f93f17c71
Max Lambrecht <max.lambrecht@hpe.com>
2021-07-22 16:27:39 -0300

    Create namespaces at the beginning
    Signed-off-by: Max Lambrecht <maxlambrecht@gmail.com>

--- POC/deploy-all.sh
+++ POC/deploy-all.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# create all namespaces at the beginning to prevent errors with the bundle sync
+./create-namespaces.sh
+
 (cd spire ; ./deploy-spire.sh)
 sleep 2
 (cd istio ; ./deploy-istio.sh)

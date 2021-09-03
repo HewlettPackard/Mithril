b6251e2f3d8df76d7c6c8aeb6eed16fe183c071e
alexandrealvino <alexandre.aaa@hotmail.com>
2021-09-03 17:05:57 -0300

    adding permissions

--- terraform/workload-to-ingress-upstream-disk/integration-tests.sh
+++ terraform/workload-to-ingress-upstream-disk/integration-tests.sh
@@ -82,7 +82,7 @@ bash -c 'kubectl rollout status deployment sleep'
 CLIENT_POD=$(kubectl get pod -l app=sleep -n default -o jsonpath="{.items[0].metadata.name}")
 
 echo $${HOST_IP}
-echo $${HOST_IP}
+#echo $${HOST_IP}
 kubectl exec -i -t pod/$CLIENT_POD -c sleep -- /bin/sh -c "curl -sSLk --cert /sleep-certs/sleep-svid.pem --key /sleep-certs/sleep-key.pem --cacert /sleep-certs/root-cert.pem https://$${HOST_IP}:8000/productpage"
 
 # Copying response to S3 bucket

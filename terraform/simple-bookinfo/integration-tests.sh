6bc883de30e9fbda22025d0d01684765f8eeb777
nathalia-satie.gomazako@hpe.com <nathalia-satie.gomazako@hpe.com>
2021-09-13 19:33:20 -0300

    [ISTIOSPIRE-85] Change integration-tests for simple-bookinfo

--- terraform/simple-bookinfo/integration-tests.sh
+++ terraform/simple-bookinfo/integration-tests.sh
@@ -53,30 +53,18 @@ docker run -i --rm \
 --network host mithril-testing:${build_tag} \
 bash -c 'kubectl rollout status deployment productpage-v1'
 
-# Request to productpage workload
-curl localhost:8000/productpage >> ${build_tag}_simple_mithril_result.txt
-
-# Copying response to S3 bucket
-#aws s3 cp /simple_mithril_${build_tag}_result.txt s3://mithril-artifacts/ --region us-east-1
-
 # Test simple_bookinfo_test
 docker run -i --rm \
 -v "/var/run/docker.sock:/var/run/docker.sock:rw" \
 -v "/.kube/config:/root/.kube/config:rw" \
 --network host mithril-testing:${build_tag} \
-<<<<<<< HEAD:terraform/simple-mithril/integration-tests.sh
-bash -c "cd /mithril/e2e && go test -v simple_bookinfo_test.go" >> ${build_tag}_simple-mithril_result.txt
+bash -c "cd /mithril/e2e && touch ${build_tag}_${usecase}_result.txt && go test -v simple_bookinfo_test.go > ${build_tag}_${usecase}_result.txt"
 
-## Copying response to S3 bucket
-#aws s3 cp ${build_tag}_simple-mithril_result.txt s3://mithril-artifacts/${build_tag}/ --region us-east-1
+# Copying response to S3 bucket
+aws s3 cp ${build_tag}_${usecase}_result.txt s3://mithril-artifacts/${build_tag}/ --region us-east-1
 
 # Generate log files
-#cat /var/log/user-data.log >> ${build_tag}_simple-mithril_result.txt
-#cp /var/log/user-data.log ${build_tag}_log.txt
-
-cat /var/log/user-data.log >> ${build_tag}_simple-mithril_log.txt
-
-aws s3 cp /${build_tag}_simple-mithril_log.txt s3://mithril-artifacts/${build_tag}/ --region us-east-1
+cat /var/log/user-data.log >> ${build_tag}_${usecase}_log.txt
 
 # Copying log to S3 bucket
 aws s3 cp /${build_tag}_${usecase}_log.txt s3://mithril-artifacts/${build_tag}/ --region us-east-1

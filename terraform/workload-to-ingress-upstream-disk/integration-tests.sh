10d58a1270cee38d6963a306461110195d86e470
alexandrealvino <alexandre.aaa@hotmail.com>
2021-09-14 17:24:06 -0300

    testing

--- terraform/workload-to-ingress-upstream-disk/integration-tests.sh
+++ terraform/workload-to-ingress-upstream-disk/integration-tests.sh
@@ -43,7 +43,7 @@ docker run -i --rm \
 -v "/var/run/docker.sock:/var/run/docker.sock:rw" \
 -v "/.kube/config:/root/.kube/config:rw" \
 --network host mithril-testing:${build_tag} \
-bash -c 'cd /mithril/usecases/workload-to-ingress-upstream-disk/server-cluster && echo DEPLOYING CLIENT CLUSTER && ./start.sh'
+bash -c 'cd /mithril/usecases/workload-to-ingress-upstream-disk/server-cluster && chmod +x start.sh && ./start.sh'
 
 ## Creating kind cluster for the server
 #docker run -i --rm \

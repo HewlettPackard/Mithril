4522bb06bbf14eca417192c457528300cced7da5
nathalia-satie.gomazako@hpe.com <nathalia-satie.gomazako@hpe.com>
2021-09-15 11:21:09 -0300

    [ISTIOSPIRE-85] Refactor workload_to_ingress test

--- e2e/go.mod
+++ e2e/go.mod
@@ -3,10 +3,22 @@ module e2e
 go 1.16
 
 require (
+	github.com/go-logr/logr v1.1.0 // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
+	github.com/google/gofuzz v1.2.0 // indirect
+	github.com/imdario/mergo v0.3.12 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/stretchr/testify v1.7.0
-	gotest.tools v2.2.0+incompatible
+	golang.org/x/net v0.0.0-20210913180222-943fd674d43e // indirect
+	golang.org/x/oauth2 v0.0.0-20210819190943-2bc19b11175f // indirect
+	golang.org/x/sys v0.0.0-20210915083310-ed5796bab164 // indirect
+	golang.org/x/term v0.0.0-20210615171337-6886f2dfbf5b // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
 	k8s.io/api v0.22.1
 	k8s.io/apimachinery v0.22.1
 	k8s.io/client-go v0.22.1
-	k8s.io/kubectl v0.22.1
+	k8s.io/klog/v2 v2.20.0 // indirect
+	k8s.io/utils v0.0.0-20210820185131-d34e5cb4466e // indirect
 )
--- e2e/workload_to_ingress_upstream_disk_test.go
+++ e2e/workload_to_ingress_upstream_disk_test.go
@@ -3,20 +3,34 @@ package e2e
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"testing"
 
-	"gotest.tools/assert"
+	"github.com/stretchr/testify/assert"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
 	rest "k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/remotecommand"
-	"k8s.io/kubectl/pkg/scheme"
 )
 
 var clientset *kubernetes.Clientset
 var kubeConfig *rest.Config
+
+type Writer struct {
+	Str []string
+}
+
+func (w *Writer) Write(p []byte) (n int, err error) {
+	str := string(p)
+	if len(str) > 0 {
+		w.Str = append(w.Str, str)
+	}
+	return len(str), nil
+}
 
 func TestWorkloadToIngressUpstreamDisk(t *testing.T) {
 	client, config, err := createClientGo()
@@ -27,67 +41,73 @@ func TestWorkloadToIngressUpstreamDisk(t *testing.T) {
 	clientset = client
 	kubeConfig = config
 
-	t.Run("deploy_poc", deployPOC)
 	t.Run("request_productpage_workload_from_sleep_pod", requestProductpageWorkloadFromSleepPod)
 }
 
 func requestProductpageWorkloadFromSleepPod(t *testing.T) {
-	labelSelector := "app=sleep"
+	labelSelector := "app=details"
 	listOptions := metav1.ListOptions{
 		LabelSelector: labelSelector,
 	}
 
-	pods, err := getPodByOptions(listOptions, clientset)
+	podList, err := clientset.CoreV1().Pods(defaultNamespace).List(context.TODO(), listOptions)
 	if err != nil {
-		t.Fatal(err)
+		fmt.Println(err)
 	}
 
-	if len(pods) == 0 {
+	if len(podList.Items) == 0 {
 		t.Fatal("Sleep pod not found")
 	}
-	sleepPod := pods[0]
-
-	hostname := getHostname()
-	command := fmt.Sprintf("echo %v\n", hostname)
+	sleepPod := podList.Items[0]
 
 	cmd := []string{
 		"sh",
 		"-c",
-		command,
+		"cat response_productpage.txt",
 	}
-	req := clientset.CoreV1().RESTClient().Post().Resource("pods").Name(sleepPod.Name).
-		Namespace(defaultNamespace).SubResource("exec")
-	execOptions := &v1.PodExecOptions{
+
+	req := clientset.CoreV1().RESTClient().Post().
+		Namespace("default").
+		Resource("pods").
+		Name(sleepPod.Name).
+		SubResource("exec").
+		Param("container", "details")
+
+	option := &v1.PodExecOptions{
 		Command: cmd,
 		Stdin:   true,
 		Stdout:  true,
 		Stderr:  true,
 		TTY:     true,
 	}
-	execOptions.Stdin = false
-	req.VersionedParams(
-		execOptions,
+	option.Stdin = false
+	req.VersionedParams(option,
 		scheme.ParameterCodec,
 	)
-	exec, err := remotecommand.NewSPDYExecutor(kubeConfig, "POST", req.URL())
-	if err != nil {
-		t.Fatal(err)
-	}
-	err = exec.Stream(remotecommand.StreamOptions{
-		Stdin:  os.Stdin,
-		Stdout: os.Stdout,
-		Stderr: os.Stderr,
-	})
-	if err != nil {
-		t.Fatal(err)
-	}
-}
 
-func deployPOC(t *testing.T) {
-	pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
+	executor, err := remotecommand.NewSPDYExecutor(kubeConfig, http.MethodPost, req.URL())
 	if err != nil {
 		t.Error(err)
 	}
 
-	assert.Equal(t, len(pods.Items), 20)
+	stdOut := new(Writer)
+	os.Stderr.Sync()
+
+	err = executor.Stream(remotecommand.StreamOptions{
+		Stdin:             nil,
+		Stdout:            stdOut,
+		Stderr:            os.Stderr,
+		Tty:               false,
+		TerminalSizeQueue: nil,
+	})
+	if err != nil {
+		t.Error(err)
+	}
+
+	fileContent, err := ioutil.ReadFile("response_productpage.txt")
+	if err != nil {
+		t.Error(err)
+	}
+
+	assert.Equal(t, stdOut.Str[0], string(fileContent))
 }

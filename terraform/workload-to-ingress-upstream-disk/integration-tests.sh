8e98d4ab32b456ecca3e5e504018416117bde2c9
alexandrealvino <alexandre.aaa@hotmail.com>
2021-09-15 17:47:35 -0300

    adding wait conditions for all apps

--- terraform/workload-to-ingress-upstream-disk/integration-tests.sh
+++ terraform/workload-to-ingress-upstream-disk/integration-tests.sh
@@ -41,11 +41,21 @@ docker run -i --rm \
 bash -c 'cd /mithril/usecases/workload-to-ingress-upstream-disk/server-cluster && find . -type f -iname "*.sh" -exec chmod +x {} \; && ./create-kind-cluster.sh &&
 HUB=${hub} AWS_ACCESS_KEY_ID=${access_key} AWS_SECRET_ACCESS_KEY=${secret_access_key} ./create-docker-registry-secret.sh &&
 kubectl create ns spire && TAG=stable_20210909 HUB=${hub} ./deploy-all.sh &&
+kubectl wait pod --for=condition=ready -l app=productpage &&
+kubectl wait pod --for=condition=Ready -l app=details &&
+kubectl wait pod --for=condition=Ready -l app=reviews &&
+kubectl wait pod --for=condition=Ready -l app=ratings &&
 INGRESS_POD=$(kubectl get pod -l app=istio-ingressgateway -n istio-system -o jsonpath="{.items[0].metadata.name}") &&
 ./forward-port.sh &&
 cd /mithril/usecases/workload-to-ingress-upstream-disk/client-cluster && find . -type f -iname "*.sh" -exec chmod +x {} \; && ./create-kind-cluster.sh &&
 HUB=${hub} AWS_ACCESS_KEY_ID=${access_key} AWS_SECRET_ACCESS_KEY=${secret_access_key} ./create-docker-registry-secret.sh &&
-TAG=stable_20210909 HUB=${hub} && ./deploy-all.sh && kubectl wait pod --for=condition=Ready -l app=sleep && kubectl rollout status deployment sleep &&
+TAG=stable_20210909 HUB=${hub} && ./deploy-all.sh &&
+kubectl wait pod --for=condition=ready -l app=sleep &&
+kubectl wait pod --for=condition=ready -l app=productpage &&
+kubectl wait pod --for=condition=Ready -l app=details &&
+kubectl wait pod --for=condition=Ready -l app=reviews &&
+kubectl wait pod --for=condition=Ready -l app=ratings &&
+kubectl rollout status deployment sleep &&
 CLIENT_POD=$(kubectl get pod -l app=sleep -n default -o jsonpath="{.items[0].metadata.name}") &&
 kubectl exec -i -t pod/$CLIENT_POD -c sleep -- /bin/sh -c "curl -sSLk --cert /sleep-certs/sleep-svid.pem --key /sleep-certs/sleep-key.pem --cacert /sleep-certs/root-cert.pem https://10.0.1.50:8000/productpage"'
 

5c76e14e9b53cb0061cbc0ea78263726d54ffb92
alexandrealvino <alexandre.aaa@hotmail.com>
2021-09-15 10:52:23 -0300

    removing wait condition in usecase

--- terraform/workload-to-ingress-upstream-disk/integration-tests.sh
+++ terraform/workload-to-ingress-upstream-disk/integration-tests.sh
@@ -41,10 +41,6 @@ docker run -i --rm \
 bash -c 'cd /mithril/usecases/workload-to-ingress-upstream-disk/server-cluster && find . -type f -iname "*.sh" -exec chmod +x {} \; && ./create-kind-cluster.sh &&
 HUB=${hub} AWS_ACCESS_KEY_ID=${access_key} AWS_SECRET_ACCESS_KEY=${secret_access_key} ./create-docker-registry-secret.sh &&
 kubectl create ns spire && TAG=stable_20210909 HUB=${hub} ./deploy-all.sh &&
-kubectl wait pod --for=condition=Ready -l app=istio-ingressgateway -n istio-system &&
-kubectl wait pod --for=condition=Ready -l app=productpage &&
-kubectl rollout status deployment productpage-v1 -n default &&
-kubectl rollout status deployment istio-ingressgateway -n istio-system &&
 INGRESS_POD=$(kubectl get pod -l app=istio-ingressgateway -n istio-system -o jsonpath="{.items[0].metadata.name}") &&
 kubectl port-forward "$INGRESS_POD" 8000:8080 -n istio-system &&
 cd /mithril/usecases/workload-to-ingress-upstream-disk/client-cluster && find . -type f -iname "*.sh" -exec chmod +x {} \; && ./create-kind-cluster.sh &&

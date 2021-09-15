37d1bcf8cf26ff7f50801a5645c33043426d842f
alexandrealvino <alexandre.aaa@hotmail.com>
2021-09-15 15:48:23 -0300

    fixing everything

--- terraform/workload-to-ingress-upstream-disk/integration-tests.sh
+++ terraform/workload-to-ingress-upstream-disk/integration-tests.sh
@@ -42,8 +42,11 @@ bash -c 'cd /mithril/usecases/workload-to-ingress-upstream-disk/server-cluster &
 HUB=${hub} AWS_ACCESS_KEY_ID=${access_key} AWS_SECRET_ACCESS_KEY=${secret_access_key} ./create-docker-registry-secret.sh &&
 kubectl create ns spire && TAG=stable_20210909 HUB=${hub} ./deploy-all.sh &&
 INGRESS_POD=$(kubectl get pod -l app=istio-ingressgateway -n istio-system -o jsonpath="{.items[0].metadata.name}") &&
-kubectl port-forward "$INGRESS_POD" 8000:8080 -n istio-system &&
-cd /mithril/usecases/workload-to-ingress-upstream-disk/client-cluster && find . -type f -iname "*.sh" -exec chmod +x {} \; && ./create-kind-cluster.sh'
+kubectl port-forward "$INGRESS_POD" 8000:8080 -n istio-system & &&
+cd /mithril/usecases/workload-to-ingress-upstream-disk/client-cluster && find . -type f -iname "*.sh" -exec chmod +x {} \; && ./create-kind-cluster.sh &&
+./deploy-all.sh && kubectl wait pod --for=condition=Ready -l app=sleep && kubectl rollout status deployment sleep &&
+CLIENT_POD=$(kubectl get pod -l app=sleep -n default -o jsonpath="{.items[0].metadata.name}") &&
+kubectl exec -i -t pod/$CLIENT_POD -c sleep -- /bin/sh -c "curl -sSLk --cert /sleep-certs/sleep-svid.pem --key /sleep-certs/sleep-key.pem --cacert /sleep-certs/root-cert.pem https://10.0.1.50:8000/productpage"'
 
 #kubectl create ns spire && TAG=stable_20210909 HUB=${hub} ./deploy-all.sh &&
 #kubectl wait pod --for=condition=Ready -l app=sleep &&

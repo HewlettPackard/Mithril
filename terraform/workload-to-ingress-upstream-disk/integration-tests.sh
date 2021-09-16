d78ecd71a97bd04e82e556548f75ce9f739739bf
alexandrealvino <alexandre.aaa@hotmail.com>
2021-09-16 18:06:04 -0300

    fixing

--- terraform/workload-to-ingress-upstream-disk/integration-tests.sh
+++ terraform/workload-to-ingress-upstream-disk/integration-tests.sh
@@ -24,17 +24,17 @@ docker run -i --rm \
 --network host mithril-testing:${build_tag} \
 bash -c 'cd /mithril/usecases/workload-to-ingress-upstream-disk/server-cluster && find . -type f -iname "*.sh" -exec chmod +x {} \; && ./create-kind-cluster.sh &&
 HUB=${hub} AWS_ACCESS_KEY_ID=${access_key} AWS_SECRET_ACCESS_KEY=${secret_access_key} ./create-docker-registry-secret.sh &&
-kubectl create ns spire && TAG=latest HUB=${hub} ./deploy-all.sh &&
+kubectl create ns spire && TAG=${build_tag} HUB=${hub} ./deploy-all.sh &&
 kubectl rollout status deployment productpage-v1 &&
 INGRESS_POD=$(kubectl get pod -l app=istio-ingressgateway -n istio-system -o jsonpath="{.items[0].metadata.name}") &&
 ./forward-port.sh &&
 cd /mithril/usecases/workload-to-ingress-upstream-disk/client-cluster && find . -type f -iname "*.sh" -exec chmod +x {} \; && ./create-kind-cluster.sh &&
 HUB=${hub} AWS_ACCESS_KEY_ID=${access_key} AWS_SECRET_ACCESS_KEY=${secret_access_key} ./create-docker-registry-secret.sh &&
-TAG=latest HUB=${hub} && ./deploy-all.sh &&
+TAG=${build_tag} HUB=${hub} && ./deploy-all.sh &&
 kubectl rollout status deployment sleep &&
 CLIENT_POD=$(kubectl get pod -l app=sleep -n default -o jsonpath="{.items[0].metadata.name}") &&
 kubectl exec -i -t pod/$CLIENT_POD -c sleep -- /bin/sh -c "curl -sSLk --cert /sleep-certs/sleep-svid.pem --key /sleep-certs/sleep-key.pem --cacert /sleep-certs/root-cert.pem https://10.0.1.50:8000/productpage > /tmp/response_productpage.txt" &&
-kubectl exec -i -t pod/$CLIENT_POD -c sleep -- /bin/sh -c "cat -e /tmp/response_productpage.txt > /tmp/response_productpage.txt" && cd /mithril/e2e && touch ${build_tag}-${usecase}-result.txt && go test -v e2e -run TestWorkloadToIngressUpstreamDisk 2>&1 | tee ${build_tag}-${usecase}-result.txt &&
+cd /mithril/e2e && touch ${build_tag}-${usecase}-result.txt && go test -v e2e -run TestWorkloadToIngressUpstreamDisk 2>&1 | tee ${build_tag}-${usecase}-result.txt &&
 AWS_ACCESS_KEY_ID=${access_key} AWS_SECRET_ACCESS_KEY=${secret_access_key} aws s3 cp ${build_tag}-${usecase}-result.txt s3://mithril-artifacts/${build_tag}/ --region us-east-1'
 
 cat /var/log/user-data.log >> ${build_tag}-${usecase}-log.txt

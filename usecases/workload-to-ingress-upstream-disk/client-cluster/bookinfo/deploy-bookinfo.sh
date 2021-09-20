02cffc35c57569bd4ee1b1075e7b41305d1e956a
alexandrealvino <alexandre.aaa@hotmail.com>
2021-09-20 12:39:28 -0300

    Testing client cluster without bookinfo deployment

--- usecases/workload-to-ingress-upstream-disk/client-cluster/bookinfo/deploy-bookinfo.sh
+++ usecases/workload-to-ingress-upstream-disk/client-cluster/bookinfo/deploy-bookinfo.sh
@@ -2,8 +2,8 @@
 
 kubectl apply -f secrets.yaml
 
-istioctl kube-inject --filename bookinfo.yaml | kubectl apply -f -
+#istioctl kube-inject --filename bookinfo.yaml | kubectl apply -f -
 
 istioctl kube-inject --filename sleep.yaml | kubectl apply -f -
 
-kubectl apply -f gateway.yaml
+#kubectl apply -f gateway.yaml

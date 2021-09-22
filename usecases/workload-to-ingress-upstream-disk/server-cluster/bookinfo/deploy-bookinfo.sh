e42d8183423f235e37b0d17b75cdc268e465778b
alexandrealvino <alexandre.aaa@hotmail.com>
2021-09-22 19:27:24 -0300

    Adding missing gateway.yaml

--- usecases/workload-to-ingress-upstream-disk/server-cluster/bookinfo/deploy-bookinfo.sh
+++ usecases/workload-to-ingress-upstream-disk/server-cluster/bookinfo/deploy-bookinfo.sh
@@ -4,4 +4,4 @@ kubectl apply -f /mithril/POC/bookinfo/secrets.yaml
 
 istioctl kube-inject --filename /mithril/POC/bookinfo/bookinfo.yaml | kubectl apply -f -
 
-kubectl apply -f /mithril/POC/bookinfo/gateway.yaml
+kubectl apply -f gateway.yaml

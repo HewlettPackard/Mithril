f05c19cafd047284734de774c3aa8c2002b24f52
alexandrealvino <alexandre.aaa@hotmail.com>
2021-10-03 19:05:43 -0300

    Updating upstream CA cert

--- usecases/workload-to-ingress-upstream-disk/client-cluster/deploy-all.sh
+++ usecases/workload-to-ingress-upstream-disk/client-cluster/deploy-all.sh
@@ -3,8 +3,8 @@
 # create all namespaces at the beginning to prevent errors with the bundle sync
 /mithril/POC/create-namespaces.sh
 
-kubectl create configmap dummy-ca-crt --from-file /mithril/usecases/workload-to-ingress-upstream-disk/server-cluster/dummy_upstream_ca.crt --namespace="spire"
-kubectl create configmap dummy-ca-key --from-file /mithril/usecases/workload-to-ingress-upstream-disk/server-cluster/dummy_upstream_ca.key --namespace="spire"
+kubectl create configmap upstream-ca-crt --from-file /mithril/usecases/workload-to-ingress-upstream-disk/server-cluster/upstream-ca.pem --namespace="spire"
+kubectl create configmap upstream-ca-key --from-file /mithril/usecases/workload-to-ingress-upstream-disk/server-cluster/upstream-ca.key.pem --namespace="spire"
 
 (cd spire ; ./deploy-spire.sh)
 sleep 2

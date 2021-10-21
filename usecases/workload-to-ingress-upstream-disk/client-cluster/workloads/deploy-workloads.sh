e9234d40ee0f451c03a1e1fa79781413b42a8133
alexandrealvino <alexandre.aaa@hotmail.com>
2021-10-21 19:13:19 -0300

    Fixing workloads path for workload-to-ingress-upstream-disk use case

--- usecases/workload-to-ingress-upstream-disk/client-cluster/workloads/deploy-workloads.sh
+++ usecases/workload-to-ingress-upstream-disk/client-cluster/workloads/deploy-workloads.sh
@@ -2,6 +2,6 @@
 
 kubectl apply -f ../../../POC/secrets.yaml
 
-istioctl kube-inject --filename ../../common/workloads/sleep.yaml | kubectl apply -f -
+istioctl kube-inject --filename ../../../common/workloads/sleep.yaml | kubectl apply -f -
 
-kubectl apply -f ../../common/networking/service-entry.yaml
+kubectl apply -f ../../../common/networking/service-entry.yaml

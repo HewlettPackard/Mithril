c031edfc1a448d277d1715b078b11f0b763e7cb1
Alexandre <alexandre.alvino@hpe.com>
2021-11-30 12:06:42 -0300

    Fixing yaml path in external-workload integration test

--- usecases/external-workload/deploy-all.sh
+++ usecases/external-workload/deploy-all.sh
@@ -3,7 +3,7 @@
 # create all namespaces at the beginning to prevent errors with the bundle sync
 ../../POC/create-namespaces.sh
 
-kubectl apply -f ../../../POC/configmaps.yaml
+kubectl apply -f ../../POC/configmaps.yaml
 
 (cd ../../POC/spire ; ./deploy-spire.sh)
 sleep 2

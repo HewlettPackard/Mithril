dd9f7f80943adee70a32a21d159c2b04b76fa287
alexandrealvino <alexandre.aaa@hotmail.com>
2021-12-20 14:08:59 -0300

    Fixes go version rollback script

--- terraform/istio-unit-tests/check-go-version.sh
+++ terraform/istio-unit-tests/check-go-version.sh
@@ -3,4 +3,4 @@
 # Check istio release version, release-1.10 has a go1.16 dependency
 
 if [[ ${istio_branch} == release-1.10 ]]; then go install golang.org/dl/go1.16.12@latest &&
-./go/bin/go1.16.12 download && export PATH="$PWD/sdk/go1.16.12/bin:$PATH" && go version; fi
+."$HOME"/go/bin/go1.16.12 download && export PATH="$HOME/sdk/go1.16.12/bin:$PATH" && go version; fi

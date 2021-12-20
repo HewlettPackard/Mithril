7ea63b8443322141f5e44ee604b8c82a21635d82
alexandrealvino <alexandre.aaa@hotmail.com>
2021-12-20 13:24:27 -0300

    Fixes go version rollback and adds documentation

--- terraform/istio-unit-tests/check-go-version.sh
+++ terraform/istio-unit-tests/check-go-version.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# Check istio release version, release-1.10 has a go1.16 dependency
+
 if [[ ${istio_branch} == release-1.10 ]]; then go install golang.org/dl/go1.16.12@latest &&
-go1.16.12 download && go version; fi
+./go/bin/go1.16.12 download && export PATH="$PWD/sdk/go1.16.12/bin:$PATH" && go version; fi

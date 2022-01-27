f276df9e5e86e8c388517f991c9fdec3a3cfffb5
nathalia-satie.gomazako@hpe.com <nathalia-satie.gomazako@hpe.com>
2022-01-27 14:35:44 -0300

    [PROW] Adjust configmap to contain github configuration

--- prow/prowjobs/istio-unit-tests.sh
+++ prow/prowjobs/istio-unit-tests.sh
@@ -2,8 +2,8 @@
 
 mkdir tmp && cd tmp
 
-git clone --single-branch --branch release-1.10 https://github.com/istio/istio.git && cd istio
-git apply /mithril/POC/patches/poc.release-1.10.patch
+git clone --single-branch --branch $BRANCH https://github.com/istio/istio.git && cd istio
+git apply /home/prow/go/src/Mithril/POC/patches/poc.$BRANCH.patch
 
 go get github.com/spiffe/go-spiffe/v2 && go mod tidy
 make build

a8c39767fb37ee11bb54922004be94ef266d1ba0
nathalia-satie.gomazako@hpe.com <nathalia-satie.gomazako@hpe.com>
2022-01-31 10:07:54 -0300

    [PROW] Adjust configmap to execute istio unit test

--- prow/prowjobs/istio-unit-tests.sh
+++ prow/prowjobs/istio-unit-tests.sh
@@ -3,7 +3,7 @@
 mkdir tmp && cd tmp
 
 git clone --single-branch --branch $BRANCH https://github.com/istio/istio.git && cd istio
-git apply /home/prow/go/src/Mithril/POC/patches/poc.$BRANCH.patch
+git apply POC/patches/poc.$BRANCH.patch
 
 go get github.com/spiffe/go-spiffe/v2 && go mod tidy
 make build

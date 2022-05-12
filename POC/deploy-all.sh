573c8228409e3548a78f75dff8bac74be6098621
Alexandre Alvino <alexandre.alvino@hpe.com>
2022-05-12 15:50:03 -0300

    Stable checkpoint
    Signed-off-by: Alexandre Alvino <alexandre.alvino@hpe.com>

--- POC/deploy-all.sh
+++ POC/deploy-all.sh
@@ -3,7 +3,10 @@
 ./create-namespaces.sh
 kubectl apply -f ./configmaps.yaml
 
-(cd spire ; ./deploy-spire.sh)
+#kubectl apply -f spire/controller/istiod.yaml
+#kubectl apply -k spire/controller
+#(cd spire ; ./deploy-spire.sh)
 
 (cd istio ; ./deploy-istio.sh)
-(cd bookinfo ; ./deploy-bookinfo.sh)
+#kubectl apply -f pilot.yaml
+#(cd bookinfo ; ./deploy-bookinfo.sh)

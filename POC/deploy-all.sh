85e674d9fb00505dc23b4503633576ebb19821a9
Alexandre Alvino <alexandre.alvino@hpe.com>
2022-05-13 11:42:58 -0300

    initial commit
    Signed-off-by: Alexandre Alvino <alexandre.alvino@hpe.com>

--- POC/deploy-all.sh
+++ POC/deploy-all.sh
@@ -3,10 +3,15 @@
 ./create-namespaces.sh
 kubectl apply -f ./configmaps.yaml
 
-#kubectl apply -f spire/controller/istiod.yaml
-#kubectl apply -k spire/controller
+kubectl apply -k spire/controller
+kubectl rollout status statefulset -n spire spire-server
+kubectl rollout status daemonset -n spire spire-agent
+kubectl apply -f spire/controller/istiod.yaml
+kubectl apply -f spire/controller/ingress-gateway.yaml
 #(cd spire ; ./deploy-spire.sh)
 
-(cd istio ; ./deploy-istio.sh)
-#kubectl apply -f pilot.yaml
+#(cd istio ; ./deploy-istio.sh)
+kubectl apply -f core.yaml
+kubectl apply -f pilot.yaml
+kubectl apply -f ingress.yaml
 #(cd bookinfo ; ./deploy-bookinfo.sh)

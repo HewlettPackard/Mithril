20c420f02a69be94b23e109b6cecb42c720b9439
Alexandre Alvino <alexandre.alvino@hpe.com>
2022-05-18 13:56:07 -0300

    Adds support for ingress and workloads for the sds approach
    Signed-off-by: Alexandre Alvino <alexandre.alvino@hpe.com>

--- POC/bookinfo/deploy-bookinfo.sh
+++ POC/bookinfo/deploy-bookinfo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-istioctl kube-inject --injectConfigFile /home/alexandre/Goland/fork/Mithril/POC/injection/inject-config.yaml \
+../istioctl2 kube-inject --injectConfigFile /home/alexandre/Goland/fork/Mithril/POC/injection/inject-config.yaml \
   --meshConfigFile /home/alexandre/Goland/fork/Mithril/POC/injection/mesh-config.yaml \
   --valuesFile /home/alexandre/Goland/fork/Mithril/POC/injection/inject-values.yaml \
   --filename bookinfo.yaml | kubectl apply -f -

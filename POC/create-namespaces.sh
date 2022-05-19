3231a26988df6c5a5cbfd5869fab1d20ede75a53
Alexandre Alvino <alexandre.alvino@hpe.com>
2022-05-19 19:11:23 -0300

    Refactoring spire folder and deploy-all.sh, updates istio-config.yaml
    Signed-off-by: Alexandre Alvino <alexandre.alvino@hpe.com>

--- POC/create-namespaces.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-kubectl create ns istio-system
-kubectl create ns spire
-sleep 2
--- POC/deploy-all.sh
+++ POC/deploy-all.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-./create-namespaces.sh
-kubectl apply -f ./configmaps.yaml
+kubectl create ns istio-system
+kubectl apply -f configmaps.yaml
 
-kubectl apply -k spire/controller
-kubectl rollout status statefulset -n spire spire-server
-kubectl rollout status daemonset -n spire spire-agent
-kubectl apply -f spire/controller/spiffe-ids.yaml
-#kubectl apply -f spire/controller/ingress-gateway.yaml
-#(cd spire ; ./deploy-spire.sh)
-
-#(cd istio ; ./deploy-istio.sh)
-kubectl apply -f core.yaml
-kubectl apply -f pilot.yaml
-kubectl apply -f ingress.yaml
+(cd spire ; ./deploy-spire.sh)
+(cd istio ; ./deploy-istio.sh)
 (cd bookinfo ; ./deploy-bookinfo.sh)

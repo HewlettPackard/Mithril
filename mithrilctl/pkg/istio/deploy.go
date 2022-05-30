6c17cbe4077b42b605cbbe0bd2ef4780e874c22e
Alexandre Alvino <alexandre.alvino@hpe.com>
2022-05-30 19:05:47 -0300

    Fix spiffeid template and enables all injection in all namespaces
    Signed-off-by: Alexandre Alvino <alexandre.alvino@hpe.com>

--- mithrilctl/pkg/istio/deploy.go
+++ mithrilctl/pkg/istio/deploy.go
@@ -15,7 +15,6 @@ func DeployIstio() {
 	if err != nil {
 		fmt.Printf("\nerror deploying istio base err: %s", err)
 	}
-	//fmt.Printf("%s", out)
 
 	command = fmt.Sprintf("install -f /home/alexandre/Goland/fork/Mithril/POC/istiod-1.13.4/istiod/values.yaml istiod /home/alexandre/Goland/fork/Mithril/POC/istiod-1.13.4/istiod/ -n istio-system --wait --timeout 120s")
 	cmdArgs = strings.Fields(command)
@@ -25,16 +24,15 @@ func DeployIstio() {
 	if err != nil {
 		fmt.Printf("\nerror deploying istiod err: %s", err)
 	}
-	//fmt.Printf("%s", out)
 
-	//command = fmt.Sprintf("rollout status deployment -n istio-system istiod")
-	//cmdArgs = strings.Fields(command)
-	//cmd = exec.New()
-	//spireInstall = cmd.Command("kubectl", cmdArgs[0:]...)
-	//out, err = spireInstall.CombinedOutput()
-	//if err != nil {
-	//	fmt.Printf("%s", err)
-	//}
+	command = fmt.Sprintf("rollout status deployment -n istio-system istiod --timeout=300s")
+	cmdArgs = strings.Fields(command)
+	cmd = exec.New()
+	spireInstall = cmd.Command("kubectl", cmdArgs[0:]...)
+	_, err = spireInstall.CombinedOutput()
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
 
 	command = fmt.Sprintf("install -f /home/alexandre/Goland/fork/Mithril/POC/gateway-1.13.4/gateway/values.yaml ingressgateway /home/alexandre/Goland/fork/Mithril/POC/gateway-1.13.4/gateway/ -n istio-system")
 	cmdArgs = strings.Fields(command)
@@ -44,14 +42,13 @@ func DeployIstio() {
 	if err != nil {
 		fmt.Printf("\nerror deploying ingressgateway err: %s", err)
 	}
-	//fmt.Printf("%s", out)
 
-	//command = fmt.Sprintf("rollout status deployment -n istio-system ingressgateway")
-	//cmdArgs = strings.Fields(command)
-	//cmd = exec.New()
-	//spireInstall = cmd.Command("kubectl", cmdArgs[0:]...)
-	//out, err = spireInstall.CombinedOutput()
-	//if err != nil {
-	//	fmt.Printf("%s", err)
-	//}
+	command = fmt.Sprintf("rollout status deployment -n istio-system ingressgateway --timeout=300s")
+	cmdArgs = strings.Fields(command)
+	cmd = exec.New()
+	spireInstall = cmd.Command("kubectl", cmdArgs[0:]...)
+	_, err = spireInstall.CombinedOutput()
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
 }

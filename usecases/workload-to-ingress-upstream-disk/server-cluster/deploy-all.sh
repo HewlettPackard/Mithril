1ff95a1de06998af5ff816ebe25952e60b8cb77c
Max Lambrecht <maxlambrecht@gmail.com>
2025-10-23 09:08:08 -0500

    Rename test private key file
    Signed-off-by: Max Lambrecht <maxlambrecht@gmail.com>

--- usecases/workload-to-ingress-upstream-disk/server-cluster/deploy-all.sh
+++ usecases/workload-to-ingress-upstream-disk/server-cluster/deploy-all.sh
@@ -6,7 +6,7 @@
 kubectl apply -f ../../../POC/configmaps.yaml
 
 kubectl create configmap upstream-ca-crt --from-file ../../common/spire/certs/upstream-ca.pem --namespace="spire"
-kubectl create configmap upstream-ca-key --from-file ../../common/spire/keys/upstream-ca.key.pem --namespace="spire"
+kubectl create configmap upstream-ca-key --from-file ../../common/spire/keys/example.key.pem --namespace="spire"
 
 (cd spire ; ./deploy-spire.sh)
 sleep 2
